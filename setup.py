f6685f82419339e115a4b5799acb7b65b4ef95e8
Antoine Augusti <antoine.augusti@gmail.com>
2018-07-04 19:46:14 +0200

    Set appropriate Python versions

--- setup.py
+++ setup.py
@@ -4,21 +4,25 @@ setup(
     name='jours_feries_france',
     license='MIT',
     packages=['jours_feries_france'],
-    version='0.1.0',
+    version='0.1.3',
     description='Get bank holidays for France',
     author='Antoine Augusti',
     author_email='hi@antoine-augusti.fr',
     url='https://github.com/AntoineAugusti/jours_feries_france',
     keywords=['bank-holidays', 'jours-feries', 'france'],
     extras_require={'dev': ['nose']},
-    python_requires='>=2.7, <4',
+    python_requires='>=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3'
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ]
 )
