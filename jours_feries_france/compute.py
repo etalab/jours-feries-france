d21b7607ff364c6c673abfe54e3c14d2ad4ec7dc
Antoine Augusti <antoine.augusti@data.gouv.fr>
2019-03-17 09:24:24 +0100

    Update algorithm for Easter

--- jours_feries_france/compute.py
+++ jours_feries_france/compute.py
@@ -21,7 +21,7 @@ class JoursFeries(object):
             "Noël": JoursFeries.noel(year),
             "Lundi de Pâques": JoursFeries.lundiDePaques(year),
             "Ascension": JoursFeries.ascension(year),
-            "Lundi de Pentecôte": JoursFeries.lundiDePentecote(year)
+            "Lundi de Pentecôte": JoursFeries.lundiDePentecote(year),
         }
 
         if include_alsace:
@@ -33,22 +33,14 @@ class JoursFeries(object):
     @staticmethod
     def paques(year):
         a = year % 19
-        b = math.floor(year / 100)
+        b = year // 100
         c = year % 100
-        d = math.floor(b / 4)
-        e = b % 4
-        f = math.floor((b + 8) / 25)
-        g = math.floor((b - f + 1) / 3)
-        h = (19 * a + b - d - g + 15) % 30
-        i = math.floor(c / 4)
-        k = c % 4
-        l = (32 + 2 * e + 2 * i - h - k) % 7
-        m = math.floor((a + 11 * h + 22 * l) / 451)
-        n0 = (h + l + 7 * m + 114)
-        n = math.floor(n0 / 31)
-        p = n0 % 31 + 1
-
-        return datetime.date(year, int(n), int(p))
+        d = (19 * a + b - b // 4 - ((b - (b + 8) // 25 + 1) // 3) + 15) % 30
+        e = (32 + 2 * (b % 4) + 2 * (c // 4) - d - (c % 4)) % 7
+        f = d + e - 7 * ((a + 11 * d + 22 * e) // 451) + 114
+        month = f // 31
+        day = f % 31 + 1
+        return datetime.date(year, month, day)
 
     @staticmethod
     def lundiDePaques(year):
@@ -92,19 +84,19 @@ class JoursFeries(object):
 
     @staticmethod
     def toussaint(year):
-            return datetime.date(year, 11, 1)
+        return datetime.date(year, 11, 1)
 
     @staticmethod
     def assomption(year):
-            return datetime.date(year, 8, 15)
+        return datetime.date(year, 8, 15)
 
     @staticmethod
     def armistice(year):
-            return datetime.date(year, 11, 11)
+        return datetime.date(year, 11, 11)
 
     @staticmethod
     def noel(year):
-            return datetime.date(year, 12, 25)
+        return datetime.date(year, 12, 25)
 
     @staticmethod
     def saintEtienne(year):
--- tests/test_compute.py
+++ tests/test_compute.py
@@ -12,35 +12,35 @@ class TestDatasetParser(unittest.TestCase):
         self.assertEquals(
             JoursFeries.for_year(2018),
             {
-                'Armistice': datetime.date(2018, 11, 11),
-                'Ascension': datetime.date(2018, 5, 10),
-                'Assomption': datetime.date(2018, 8, 15),
-                'Fête Nationale': datetime.date(2018, 7, 14),
-                'Fête du travail': datetime.date(2018, 5, 1),
+                "Armistice": datetime.date(2018, 11, 11),
+                "Ascension": datetime.date(2018, 5, 10),
+                "Assomption": datetime.date(2018, 8, 15),
+                "Fête Nationale": datetime.date(2018, 7, 14),
+                "Fête du travail": datetime.date(2018, 5, 1),
                 "Jour de l'an": datetime.date(2018, 1, 1),
-                'Lundi de Pâques': datetime.date(2018, 4, 2),
-                'Noël': datetime.date(2018, 12, 25),
-                'Lundi de Pentecôte': datetime.date(2018, 5, 21),
-                'Toussaint': datetime.date(2018, 11, 1),
-                'Victoire des alliés': datetime.date(2018, 5, 8)
-            }
+                "Lundi de Pâques": datetime.date(2018, 4, 2),
+                "Noël": datetime.date(2018, 12, 25),
+                "Lundi de Pentecôte": datetime.date(2018, 5, 21),
+                "Toussaint": datetime.date(2018, 11, 1),
+                "Victoire des alliés": datetime.date(2018, 5, 8),
+            },
         )
 
         self.assertEquals(
             JoursFeries.for_year(2020),
             {
-                'Armistice': datetime.date(2020, 11, 11),
-                'Ascension': datetime.date(2020, 5, 21),
-                'Assomption': datetime.date(2020, 8, 15),
-                'Fête Nationale': datetime.date(2020, 7, 14),
-                'Fête du travail': datetime.date(2020, 5, 1),
+                "Armistice": datetime.date(2020, 11, 11),
+                "Ascension": datetime.date(2020, 5, 21),
+                "Assomption": datetime.date(2020, 8, 15),
+                "Fête Nationale": datetime.date(2020, 7, 14),
+                "Fête du travail": datetime.date(2020, 5, 1),
                 "Jour de l'an": datetime.date(2020, 1, 1),
-                'Lundi de Pâques': datetime.date(2020, 4, 13),
-                'Noël': datetime.date(2020, 12, 25),
-                'Lundi de Pentecôte': datetime.date(2020, 6, 1),
-                'Toussaint': datetime.date(2020, 11, 1),
-                'Victoire des alliés': datetime.date(2020, 5, 8)
-            }
+                "Lundi de Pâques": datetime.date(2020, 4, 13),
+                "Noël": datetime.date(2020, 12, 25),
+                "Lundi de Pentecôte": datetime.date(2020, 6, 1),
+                "Toussaint": datetime.date(2020, 11, 1),
+                "Victoire des alliés": datetime.date(2020, 5, 8),
+            },
         )
 
     def test_for_year_in_alsace(self):
@@ -48,37 +48,42 @@ class TestDatasetParser(unittest.TestCase):
         self.assertEquals(
             JoursFeries.for_year(2018, include_alsace=True),
             {
-                'Armistice': datetime.date(2018, 11, 11),
-                'Ascension': datetime.date(2018, 5, 10),
-                'Assomption': datetime.date(2018, 8, 15),
-                'Fête Nationale': datetime.date(2018, 7, 14),
-                'Fête du travail': datetime.date(2018, 5, 1),
+                "Armistice": datetime.date(2018, 11, 11),
+                "Ascension": datetime.date(2018, 5, 10),
+                "Assomption": datetime.date(2018, 8, 15),
+                "Fête Nationale": datetime.date(2018, 7, 14),
+                "Fête du travail": datetime.date(2018, 5, 1),
                 "Jour de l'an": datetime.date(2018, 1, 1),
-                'Lundi de Pâques': datetime.date(2018, 4, 2),
-                'Noël': datetime.date(2018, 12, 25),
-                'Lundi de Pentecôte': datetime.date(2018, 5, 21),
-                'Toussaint': datetime.date(2018, 11, 1),
-                'Victoire des alliés': datetime.date(2018, 5, 8),
-                'Vendredi Saint': datetime.date(2018, 3, 30),
-                'Saint Étienne': datetime.date(2018, 12, 26)
-            }
+                "Lundi de Pâques": datetime.date(2018, 4, 2),
+                "Noël": datetime.date(2018, 12, 25),
+                "Lundi de Pentecôte": datetime.date(2018, 5, 21),
+                "Toussaint": datetime.date(2018, 11, 1),
+                "Victoire des alliés": datetime.date(2018, 5, 8),
+                "Vendredi Saint": datetime.date(2018, 3, 30),
+                "Saint Étienne": datetime.date(2018, 12, 26),
+            },
         )
 
         self.assertEquals(
             JoursFeries.for_year(2020, include_alsace=True),
             {
-                'Armistice': datetime.date(2020, 11, 11),
-                'Ascension': datetime.date(2020, 5, 21),
-                'Assomption': datetime.date(2020, 8, 15),
-                'Fête Nationale': datetime.date(2020, 7, 14),
-                'Fête du travail': datetime.date(2020, 5, 1),
+                "Armistice": datetime.date(2020, 11, 11),
+                "Ascension": datetime.date(2020, 5, 21),
+                "Assomption": datetime.date(2020, 8, 15),
+                "Fête Nationale": datetime.date(2020, 7, 14),
+                "Fête du travail": datetime.date(2020, 5, 1),
                 "Jour de l'an": datetime.date(2020, 1, 1),
-                'Lundi de Pâques': datetime.date(2020, 4, 13),
-                'Noël': datetime.date(2020, 12, 25),
-                'Lundi de Pentecôte': datetime.date(2020, 6, 1),
-                'Toussaint': datetime.date(2020, 11, 1),
-                'Victoire des alliés': datetime.date(2020, 5, 8),
-                'Vendredi Saint': datetime.date(2020, 4, 10),
-                'Saint Étienne': datetime.date(2020, 12, 26)
-            }
+                "Lundi de Pâques": datetime.date(2020, 4, 13),
+                "Noël": datetime.date(2020, 12, 25),
+                "Lundi de Pentecôte": datetime.date(2020, 6, 1),
+                "Toussaint": datetime.date(2020, 11, 1),
+                "Victoire des alliés": datetime.date(2020, 5, 8),
+                "Vendredi Saint": datetime.date(2020, 4, 10),
+                "Saint Étienne": datetime.date(2020, 12, 26),
+            },
         )
+
+    def testPaques(self):
+        self.assertEquals(JoursFeries.paques(1954), datetime.date(1954, 4, 18))
+        self.assertEquals(JoursFeries.paques(1981), datetime.date(1981, 4, 19))
+        self.assertEquals(JoursFeries.paques(2049), datetime.date(2049, 4, 18))
